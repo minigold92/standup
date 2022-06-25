c9871996c5a6a41f1d6509c0e15927bc28c56aba
mgh23 <minigold92@gmail.com>
2022-06-25 23:48:40 +0900

    day1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Stand up 앱을 만들어 봅시다.</h2>
+      </header>
+      <p className="App-intro">여기서부터 프로젝트 시작</p>
+    </div>
+  );
+}
+
+export default App;
