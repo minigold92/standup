afdb937bdc1b5f65657a32667642f4dd7227e4b6
mgh23 <minigold92@gmail.com>
2022-06-27 20:31:45 +0900

    day3

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import ReactDOM from "react-dom/client";
+import React from "react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+it("renders without chrashing", () => {
+  const root = ReactDOM.createRoot(document.createElement("div"));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 });
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -3,15 +3,64 @@ import "./Editor.css";
 import Profile from "./Profile";
 
 class Editor extends Component {
+  constructor(props) {
+    super(props);
+    this.onPaste = this.onPaste.bind(this);
+    this.editorChange = this.editorChange.bind(this);
+    this.getCard = this.getCard.bind(this);
+    this.hasValue = this.hasValue.bind(this);
+
+    this.state = { embedlyUrl: undefined, content: undefined };
+  }
+
+  onPaste(event) {
+    event.clipboardData.items[0].getAsString((text) => {
+      if (this.detectURL(text)) {
+        this.setState({ embedlyUrl: text, content: this.state.content });
+      }
+    });
+  }
+  editorChange(event) {
+    let checkText = this.detectURL(event.currentTarget.textContent);
+    if (!this.state.embedlyUrl && (event.keyCode === 32 || event.keyCode === 13) && checkText) {
+      this.setState({ embedlyUrl: checkText, content: event.currentTarget.textContent });
+    } else {
+      this.setState({ content: event.currentTarget.textContent });
+    }
+  }
+  getCard(embedlyUrl) {
+    return <div>{embedlyUrl || ""}</div>;
+  }
+  detectURL(text) {
+    const urls = text.match(/(https?:\/\/[^\s]+)/g) || text.match(/www.[^\s]+/g);
+    if (urls.length > 0) return urls[0];
+    return undefined;
+  }
+
+  hasValue(value) {
+    if (value && typeof value === "string") return !!value.trim();
+    return false;
+  }
+  handleSubmit(event) {
+    this.props.submit();
+  }
+
   render() {
     return (
       <div className="wrapEditor">
         <Profile isAnonymous={this.props.isAnonymous} />
         <div className="textEditor">
-          <div className="innerEdit" contentEditable="true" placeholder="글쓰기..."></div>
+          <div
+            className="innerEdit"
+            contentEditable="true"
+            placeholder="글쓰기..."
+            onPaste={this.onPaste}
+            onKeyUp={this.editorChange}
+          ></div>
+          {this.getCard(this.state.embedlyUrl)}
         </div>
         <div className="actionBar">
-          <button className="upload" onClick={this.props.handleSubmit}>
+          <button className="upload" disable={!this.hasValue(this.state.content)} onClick={this.props.handleSubmit}>
             <span>스탠드업!</span>
           </button>
         </div>
